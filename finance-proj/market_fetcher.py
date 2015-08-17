5a9fd24e7b0f286a3eafe6610879e905d74e4d9a
Aidan Gomez <agomez@venturemedia.com>
2015-08-16 20:32:23 -0700

    Swap 2 misnamed files and convert market request to class

--- finance-proj/market_fetcher.py
+++ finance-proj/market_fetcher.py
@@ -1,60 +1,17 @@
 import requests
-import io
+import market-fetcher
 
-from market_exceptions import *
+def stockQuote(symbol, start_date, end_date):
+    stockList = send(services["search_symbol"], params={"prefix":symbol})
+    stockId = stockList[0]["symbolId"]
 
+    startDate = start_date + "T00:00:00-05:00"
+    endDate = end_date + "T00:00:00-05:00"
 
-tokenFile = io.open("tokeninfo.txt")
-key = tokenFile.readline().strip()
-refreshToken = tokenFile.readline().strip()
-server = tokenFile.readline().strip()
-tokenFile.close()
-
-headers = lambda key: {"Authorization": "Bearer " + key}
-
-services = {"search_symbol":"v1/symbols/search", "stock_info":"v1/symbols/", "get_quotes":"v1/makets/candles/"}
-
-def checkRequest(request):
-     code = request.status_code
-     if (code == 200):
-         return 1
-     elif (code == 202):
-         return 1
-     elif (code == 401):
-         raise AccessTokenError("bad access token")
-     elif (code == 400):
-         raise BadRequestError("bad request")
-     elif (code == 404):
-         raise InvalidEndpointError("invalid endpoint")
-     else:
-         print("ERROR: " + str(code))
-         print(request.text)
-         raise Exception
-
-def refreshAuthentication():
-    global key
-    global refreshToken
-    global server
-    r = requests.get("https://login.questrade.com/oauth2/token", params={"grant_type":"refresh_token", "refresh_token":refreshToken})
-    if (checkRequest(r)):
-        tokenFile = io.open("tokeninfo.txt", mode="wt")
-        key = r.json()["access_token"]
-        refreshToken = r.json()["refresh_token"]
-        server = r.json()["api_server"]
-        tokenFile.writelines('\n'.join([key, refreshToken, server]))
-        tokenFile.close()
-    return r
-
-def send(service, params = None):
-    r = requests.get(server + service, headers=headers(key))
-    try:
-        checkRequest(r)
-    except AccessTokenError:
-        refreshAuthentication()
-        r = requests.get(server + service, headers=headers(key))
-    return r
-
+    return send(services["get_quotes"], params={"id":stockId, "startTime":startDate, "endTime":endDate, "interval":"FiveMinutes"})
 
 if __name__ == "__main__":
-    response = send("v1/accounts")
+    # response = a.refresh_authentication()
+    # print(response.text)
+    response = send("accounts")
     print(response.text)
