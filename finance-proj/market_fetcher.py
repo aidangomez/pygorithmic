e1aaff93c9ea331e0af0d848e5fd080b6caf060e
Aidan Gomez <aidan.n.gomez@gmail.com>
2015-08-19 21:48:16 -0700

    Fix errors in market fetcher

--- finance-proj/market_fetcher.py
+++ finance-proj/market_fetcher.py
@@ -4,7 +4,7 @@ import market_request
 from Resource.enumerations import Time, RequestType
 from Resource.objects import Quote
 
-def stockQuote(symbol, startDate, endDate, timeFrame):
+def stockQuotes(symbol, startDate, endDate, timeFrame):
     """
     dates povided in string as "year-month-day"
     """
@@ -15,22 +15,26 @@ def stockQuote(symbol, startDate, endDate, timeFrame):
     startDate = startDate + "T00:00:00-05:00"
     endDate = endDate + "T00:00:00-05:00"
 
-    return request.send(RequestType.getQuotes + str(stockId),\
-        params={"startTime":startDate, "endTime":endDate, "interval":timeFrame})
+    return request.send(RequestType.getQuotes + str(stockId),
+        params={"startTime":startDate,
+                "endTime":endDate,
+                "interval":timeFrame})["candles"]
 
 def populateDB(stockname, quotes):
     connection = sqlite3.connect("quotes.db")
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS ? (start, end, low, high, open,\
-        close, volume)", (stockname))
+    cursor.execute("CREATE TABLE IF NOT EXISTS {} (start, end, low, high, open,\
+        close, volume)".format(stockname))
 
     quoteList = []
     for quote in quotes:
         tempQuote = Quote(quote)
         quoteList.append(tempQuote)
 
-        cursor.execute("INSERT INTO ? VALUES (?, ?, ?, ?, ?, ?, ?)",\
-            (stockname, tempQuote.start, tempQuote.end, tempQuote.low,\
+        cursor.execute("INSERT INTO {} VALUES (?, ?, ?, ?, ?, ?, ?)".format(stockname),
+            (tempQuote.start, tempQuote.end, tempQuote.low,
              tempQuote.high, tempQuote.open, tempQuote.close, tempQuote.volume))
-    
+
+    connection.commit()
+    cursor.close()
     connection.close()
