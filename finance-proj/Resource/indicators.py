5f999aaab7ac2dd27a14d4cae0273eb1201d844e
Aidan Gomez <aidan.n.gomez@gmail.com>
2015-08-19 21:49:02 -0700

    Add RSI and Stochastic to indicators

--- finance-proj/Resource/indicators.py
+++ finance-proj/Resource/indicators.py
@@ -1,4 +1,5 @@
 """
+Big thank you to pylab for these implementations.
 http://matplotlib.org/examples/pylab_examples/finance_work2.html
 """
 
@@ -10,7 +11,6 @@ def exponentialMovingAverage(x, n):
     """
     x = np.array(x)
 
-    # initialize exponential weights
     weights = np.exp(np.linspace(-1.0, 0.0, n))
     weights /=  sum(weights)
 
@@ -23,3 +23,42 @@ def movingAverageConvergenceDivergence(x, signalTerm=9, fastTerm=12, slowTerm=26
     fast = exponentialMovingAverage(x, fastTerm)
     signal = exponentialMovingAverage(fast - slow, signalTerm)
     return (fast - slow, signal)
+
+def relativeStrength(x, n=14):
+    deltas = np.diff(x)
+    seed = deltas[:n+1]
+    up = seed[seed>=0].sum()/n
+    down = -seed[seed<0].sum()/n
+    rs = up/down
+    rsi = np.zeros_like(x)
+    rsi[:n] = 100. - 100./(1.+rs)
+    for i in range(n, len(x)):
+        delta = deltas[i-1]
+        if delta>0:
+            upval = delta
+            downval = 0.
+        else:
+            upval = 0.
+            downval = -delta
+        up = (up*(n-1) + upval)/n
+        down = (down*(n-1) + downval)/n
+        rs = up/down
+        rsi[i] = 100. - 100./(1.+rs)
+    return rsi
+
+def stochastic(x, n=14):
+    x = np.array(x)
+    stoch = np.zeros_like(x)
+    high = np.amax(x[:n])
+    low = np.amin(x[:n])
+    for i in range(n, len(x)):
+        if (high < x[i] and x[i-n-1] < high):
+            high = x[i]
+        elif (x[i-n-1] == high):
+            high = np.amax(x[i-n:i])
+        if (low > x[i] and x[i-n-1] > low):
+            low = x[i]
+        elif (x[i-n-1] == low):
+            low = np.amin(x[i-n:i])
+        stoch[i] = 100 * (x[i] - low) / (high - low)
+    return stoch
