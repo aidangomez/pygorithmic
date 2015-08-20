3f35742d4cf6168e9156a6ab91d63b861193a7cb
Aidan Gomez <aidan.n.gomez@gmail.com>
2015-08-19 21:52:01 -0700

    Add centralized testing method

---
+++ finance-proj/Debug/main_testing.py
@@ -0,0 +1,8 @@
+import plot_graph
+import ..market_fetcher
+import ..market_request
+import ..account_manager.py
+import ..Resource.enumerations
+import ..Resource.indicators
+import ..Resource.market_exceptions
+import ..Resource.objects
