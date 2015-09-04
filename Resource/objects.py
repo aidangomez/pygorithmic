d7313e1f3ca56a24cadf8316c7ca15708ce2faa4
Aidan Gomez <agomez@venturemedia.com>
2015-09-03 20:19:03 -0700

    Add Time object and algorithm testing framework

--- Resource/objects.py
+++ Resource/objects.py
@@ -1,5 +1,4 @@
 class Quote:
-
     def __init__(self, quote):
         self.start = quote["start"]
         self.end = quote["end"]
@@ -8,3 +7,8 @@ class Quote:
         self.open = quote["open"]
         self.close = quote["close"]
         self.volume = quote["volume"]
+
+class Time(str):
+    def __new__(cls, year, month, day, hour="00", min="00", sec="00"):
+        string = '-'.join([year, month, day]) + "T" + ":".join([hour, min, sec]) + "-05:00"
+        return str.__new__(cls, string)
