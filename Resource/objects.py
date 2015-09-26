98e8ccaef7176bca2c5bb8847d0115517c1bde01
Aidan Gomez <agomez@venturemedia.com>
2015-09-26 14:44:50 -0700

    Improve Time object

--- Resource/objects.py
+++ Resource/objects.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import datetime, tzinfo
 
-from enumerations import Interval
+from Resource.enumerations import Interval
 
 
 class Quote:
@@ -17,13 +17,38 @@ class Quote:
 
 class Time:
 
-    def __init__(self, year, month, day, hour="00", min="00", sec="00",
-                 interval=Interval.OneDay.delta):
-        self.date = datetime(year, month, day, hour, min, second)
-        self.delta = interval
+    def __init__(self, year, month, day, hour=0, min=0, sec=0,
+                 interval=Interval.OneDay):
+        self.date = datetime(year, month, day, hour, min, sec)
+        self.interval = interval
 
     def __repr__(self):
         return self.date.isoformat() + "-05:00"
 
+    def __add__(self, b):
+        newDate = self.date + b * self.interval.delta
+        newTime = Time(1, 1, 1, interval=self.interval)
+        newTime.date = newDate
+        return newTime
+
     def __iadd__(self, b):
-        self.date += b * self.delta
+        self.date += b * self.interval.delta
+        return self
+
+    def __lt__(self, b):
+        return self.date < b
+
+    def __le__(self, b):
+        return self.date <= b
+
+    def __eq__(self, b):
+        return self.date == b
+
+    def __ne__(self, b):
+        return self.date != b
+
+    def __gt__(self, b):
+        return self.date > b
+
+    def __ge__(self, b):
+        return self.date >= b
--- market_fetcher.py
+++ market_fetcher.py
@@ -5,17 +5,13 @@ from Resource.enumerations import Interval, RequestType
 from Resource.objects import Quote
 
 
-def stockQuotes(symbol, startDate, endDate, interval):
-    """
-    interval is an Resource.enumerations.Interval
-
-    """
+def stockQuotes(symbol, startDate, endDate):
     request = questrade_request.MarketRequest()
     stockList = request.get(RequestType.Market.searchSymbol,
                             params={"prefix": symbol})
     stockId = stockList["symbols"][0]["symbolId"]
 
     return request.get(RequestType.Market.getQuotes(stockId),
-                       params={"startTime": startDate,
-                               "endTime": endDate,
-                               "interval": interval.string})["candles"]
+                       params={"startTime": str(startDate),
+                               "endTime": str(endDate),
+                               "interval": startDate.interval.string})["candles"]
