d49f23f948fe7f25026121a0e575a4f8ce320533
Aidan Gomez <agomez@venturemedia.com>
2015-11-11 18:13:41 -0800

    Prevent database duplicate population

--- Database/populate_database.py
+++ Database/populate_database.py
@@ -1,12 +1,21 @@
 from datetime import datetime
 
 from Database import database
+from Resource.enumerations import SortingOrder
+from Resource.objects import Time
 from Questrade import market_fetcher
 
 
 def populate(db, symbol, startDate):
     columns = ["timestamp", "open", "close", "high", "low", "volume"]
     db.createTable(symbol, columns)
+
+    db.sort(symbol, "timestamp", SortingOrder.Descending)
+    latestTimeStamp = db.next()["timestamp"]
+    latestTime = Time(timestamp=latestTimeStamp)
+
+    if latestTime > startDate:
+        startDate = latestTime + 1
 
     endDate = startDate + 1000
     while (endDate < datetime.now()):
@@ -18,7 +27,8 @@ def populate(db, symbol, startDate):
             high = quote["high"]
             low = quote["low"]
             volume = quote["volume"]
-            db.insert(symbol, [timestamp, open, close, high, low, volume])
+            db.insertUnique(symbol, [timestamp, open, close, high, low,
+                                     volume])
         startDate += 1000
         endDate += 1000
 
@@ -31,6 +41,6 @@ def populate(db, symbol, startDate):
         high = quote["high"]
         low = quote["low"]
         volume = quote["volume"]
-        db.insert(symbol, [timestamp, open, close, high, low, volume])
+        db.insertUnique(symbol, [timestamp, open, close, high, low, volume])
 
     db.sort(symbol, "timestamp")
