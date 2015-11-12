6a7a739e393ef979534a8cf19d872f251e13c1a1
Aidan Gomez <agomez@venturemedia.com>
2015-11-11 18:04:06 -0800

    Add simple MACD and RSI sample algorithms

--- Debug/sample_algo.py
+++ Debug/sample_algo.py
@@ -1,6 +1,8 @@
 from Resource.algorithm import Algorithm
 from Resource.enumerations import AlgorithmResponse
 
+from Resource import indicators
+
 
 class SampleAlgo(Algorithm):
     def advise(self, data):
@@ -11,5 +13,42 @@ class SampleAlgo(Algorithm):
             return AlgorithmResponse.Buy
         elif (data[-1] < data[0]):
             return AlgorithmResponse.Sell
+        else:
+            return AlgorithmResponse.Hold
+
+
+class MACDAlgo(Algorithm):
+    def __init__(self, initialData):
+        super().__init__(26)
+        self.storedData = initialData
+
+    def advise(self, data):
+        self.storedData += data
+        (MACD, signal) = indicators.movingAverageConvergenceDivergence(
+                            self.storedData)
+        MACDslope = MACD[-1] - MACD[-2]
+
+        if (MACDslope > 0) & (MACD[-1] < signal[-1]):
+            return AlgorithmResponse.Buy
+        elif (MACDslope < 0) & (MACD[-1] > signal[-1]):
+            return AlgorithmResponse.Sell
+        else:
+            return AlgorithmResponse.Hold
+
+
+class RSIAlgo(Algorithm):
+    def __init__(self, initialData):
+        super().__init__(26)
+        self.storedData = initialData
+
+    def advise(self, data):
+        self.storedData += data
+        RSI = Resource.indicators.relativeStrength()
+        RSIValue = MACD[-1]
+
+        if RSIValue > 70:
+            return AlgorithmResponse.Sell
+        elif RSIValue < 30:
+            return AlgorithmResponse.Buy
         else:
             return AlgorithmResponse.Hold
