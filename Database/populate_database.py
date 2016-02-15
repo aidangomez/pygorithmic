505f2f2a0f8fee38344992b026595a3f7838f767
Aidan Gomez <agomez@venturemedia.com>
2016-02-14 16:36:48 -0800

    Add market data convenience method

--- Database/populate_database.py
+++ Database/populate_database.py
@@ -18,11 +18,7 @@ def populate(db, symbol, start_date, end_date=Time.now()):
         if latest_time > start_date:
             start_date = latest_time + 1
 
-    batch_start_time = start_date
-    batch_end_time = start_date + 1000
-    while (batch_end_time < end_date):
-        quotes = market_fetcher.stock_quotes(symbol, batch_start_time,
-                                             batch_end_time)
+    def add_to_database(quotes):
         for quote in quotes:
             timestamp = quote["start"]
             open = quote["open"]
@@ -32,20 +28,8 @@ def populate(db, symbol, start_date, end_date=Time.now()):
             volume = quote["volume"]
             db.insert_unique(symbol, [timestamp, open, close, high, low,
                                       volume])
-        batch_start_time += 1000
-        batch_end_time += 1000
 
-    if batch_start_time < end_date:
-        quotes = market_fetcher.stock_quotes(symbol, batch_start_time,
-                                             end_date)
-        for quote in quotes:
-            timestamp = quote["start"]
-            open = quote["open"]
-            close = quote["close"]
-            high = quote["high"]
-            low = quote["low"]
-            volume = quote["volume"]
-            db.insert_unique(symbol, [timestamp, open, close, high, low,
-                                      volume])
+    fetch_market_data(symbol, start_date, end_date=end_date,
+                      action=add_to_database)
 
     db.sort(symbol, "timestamp")
--- Questrade/market_fetcher.py
+++ Questrade/market_fetcher.py
@@ -2,7 +2,7 @@ import sqlite3
 
 from Questrade import questrade_request
 from Resource.enumerations import Interval, RequestType
-from Resource.objects import Quote
+from Resource.objects import Time, Quote
 
 
 def stock_quotes(symbol, start_date, end_date):
@@ -15,3 +15,20 @@ def stock_quotes(symbol, start_date, end_date):
                                "endTime": str(end_date),
                                "interval": start_date.interval.string
                                })["candles"]
+
+def fetch_market_data(symbol, start_date, end_date=Time.now(),
+                      action=lambda x: return):
+    batch_start_time = start_date
+    batch_end_time = start_date + 1000
+    while (batch_end_time < end_date):
+        quotes = stock_quotes(symbol, batch_start_time,
+                                             batch_end_time)
+        action(quotes)
+        
+        batch_start_time += 1000
+        batch_end_time += 1000
+
+    if batch_start_time < end_date:
+        quotes = stock_quotes(symbol, batch_start_time,
+                                             end_date)
+        action(quotes)
